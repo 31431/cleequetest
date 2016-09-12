8e5f834af5bbe9bdfde78920a65bd9d4a0e045ea
Krittin Kawkeeree <kitsada280@gmail.com>
2016-09-12 12:27:11 +0800

    Updating changes to approvingMember.php and method to approve member

--- approvingMember.php
+++ approvingMember.php
@@ -1,15 +1,13 @@
 <?php
 	session_start();
-	$_POST['']
 	$groupID = $_SESSION['groupID'];
-	$username = $_SESSION['username'];
 	include("groupFunction.php");
 	include("main_ics_processer.php");
 
 
-	foreach($_POST['userChosen'] as $key=>$userID){
+	foreach($_POST['userApproved'] as $key=>$userID){
 		$username = gettingUsernameFromID($userID);
-		changePendingToZero ($groupID,$username);
+		changePendingToZero($groupID,$username);
 	}
 
 	header('Location: https://cleequetest.herokuapp.com/addmember.php?groupNameSelected='.$groupID.'&submit=Go%21');
--- groupFunction.php
+++ groupFunction.php
@@ -155,12 +155,12 @@ function printingGroupMember($groupID){
 function printingPendingPeople($groupID){
 	$userArray = gettingPendingPeople($groupID);
 	if(count($userArray) != 0){
-		echo "<form action='approvingMember.php' method='post' name='userChosen'>";
+		echo "<form action='approvingMember.php' method='post' name='userApproved'>";
 		foreach($userArray as $key=>$value){
 			foreach ($value as $subkey => $userID) {
 				$name= gettingUsernameFromID($userID);
 				$fullname = gettingNameFromUsername($name);
-				echo "<input type='checkbox' class='member' name='userChosen[$name]' value='$userID' id='$name' style='display:none'></input>";
+				echo "<input type='checkbox' class='member' name='userApproved[$name]' value='$userID' id='$name' style='display:none'></input>";
 				echo "<label for='$name' class='groupPeopleDep'><p style='margin:0;'>$name</p><p class='nameDescription' style='color:#999999'>$fullname</p></label>";
 			}
 		}
